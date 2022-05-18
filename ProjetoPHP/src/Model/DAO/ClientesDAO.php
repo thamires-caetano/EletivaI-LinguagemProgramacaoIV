4dc7474100dbd22df46ffba5c7e34461a2ca32c6
vanessaborges2 <65421224+vanessaborges2@users.noreply.github.com>
2022-05-18 20:25:22 -0300

    Aula 18-05

---
+++ ProjetoPHP/src/Model/DAO/ClientesDAO.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Aluno\ProjetoPhp\Model\DAO;
+
+use Aluno\ProjetoPhp\Model\Entity\Clientes;
+
+class ClientesDAO{
+
+    public function inserir(Clientes $c){
+        try{
+            $sql = "INSERT INTO `clientes`(`nome`, `email`, `idade`) VALUES (:nome, :email, :idade)";
+            
+
+        } catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function alterar(Clientes $c){
+        try{
+            $sql = "UPDATE `clientes` SET `nome`=:nome,`email`=:email,`idade`=:idade WHERE id = :id";
+            
+
+        } catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function excluir($id){
+        try{
+            $sql = "DELETE FROM `clientes` WHERE id = :id";
+            
+
+        } catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function consultar(){
+        try{
+            $sql = "SELECT * FROM clientes";
+            
+
+        } catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function consultarPorId($id){
+        try{
+            $sql = "SELECT * FROM clientes WHERE id = :id";
+            
+
+        } catch(\Exception $e){
+            return false;
+        }
+    }
+
+}
